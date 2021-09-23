5e1ef075d0ab436b2612fb41f2a63039c77f6f95
acaldow <arnaud.caldow@gmail.com>
2021-09-23 21:46:55 +0200

    Added aggregated DF

--- src/main/scala/DotaETL.scala
+++ src/main/scala/DotaETL.scala
@@ -33,25 +33,42 @@ object DotaETL extends App {
 
   val request2: HttpRequest = Http(s"https://api.opendota.com/api/players/$account_id")
   val response2 = request2.asString
-  val playerDS = Seq(response2.body).toDS()
-  val playerDF = spark.read.json(playerDS).select("profile.account_id", "profile.name")
+  val nameDS = Seq(response2.body).toDS()
+  val nameDF = spark.read.json(nameDS).select("profile.account_id", "profile.name")
 
-  val finalDF = accountDF.join(playerDF, "account_id")
+  val playerDF = accountDF.join(nameDF, "account_id")
 
-  val myList = finalDF.select("match_id").map(f => f.getLong(0)).collectAsList()
+  val myList = playerDF.select("match_id").map(f => f.getLong(0)).collectAsList()
   val func = (x: Long) => Http(s"https://api.opendota.com/api/matches/$x").asString.body
   val matchList = myList.map(func)
   val matchDS = matchList.toList.toDS()
   val matchDF = spark.read.json(matchDS)
 
-  val w = Window.partitionBy("players.isRadiant")
-  val parsedDF = matchDF.select(explode($"players").alias("players")).select(
+  val w = Window.partitionBy("match_id", "players.isRadiant")
+  val parsedDF = matchDF.select($"match_id", explode($"players").alias("players")).select(
+    $"match_id",
     $"players.account_id",
     $"players.isRadiant",
     $"players.kills",
     $"players.assists",
-    sum("players.kills").over(w).alias("Total Kills"))
-  parsedDF.show(10, false)
+    sum("players.kills").over(w).alias("total_team_kills"))
+  val kpDF = parsedDF.select(
+    $"match_id",
+    $"account_id",
+    when($"isRadiant" === true, "Radiant").otherwise("Dire").alias("team"),
+    ((($"kills" + $"assists")/$"total_team_kills")*100).alias("KP"))
+
+  val summaryDF = playerDF.join(kpDF, Seq("account_id", "match_id"))
+  val aggregatedDF = summaryDF.groupBy("name")
+    .agg(
+      count("match_id").as("total_games"),
+      max("KDA").as("max_kda"),
+      min("KDA").as("min_kda"),
+      avg("KDA").as("avg_kda"),
+      max("KP").as("max_kp"),
+      min("KP").as("min_kp"),
+      avg("KP").as("avg_kp"))
+  aggregatedDF.show(false)
 
   spark.stop()
   System.exit(0)
